42cba3501f608e7db288569f2987f871c976b6f4
Diego Alejandro Medina Martinez <diego2303medinam@gmail.com>
2026-03-27 23:34:09 -0500

    feat: expose Swagger and ReDoc documentation in all environments

--- main.py
+++ main.py
@@ -27,10 +27,8 @@ logger = logging.getLogger(__name__)
 app = FastAPI(
     title="Facturas API",
     version="0.1.0",
-    # Swagger and ReDoc are only exposed in development.
-    # In production, disable them to avoid leaking schema information.
-    docs_url="/docs" if settings.is_development else None,
-    redoc_url="/redoc" if settings.is_development else None,
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
 # ---------------------------------------------------------------------------
